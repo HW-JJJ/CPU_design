98d779645f97794f0d172bb71476a434770f0408
정현우 <erios5774@gmail.com>
2025-04-23 17:18:18 +0900

    Add files via upload

---
+++ 0423/FND_COUNT/Compiler Explorer C Editor #1 Code 0423.c
@@ -0,0 +1,82 @@
+#include <stdint.h>
+
+#define __IO         volatile
+
+typedef struct {
+    __IO uint32_t MODER;
+    __IO uint32_t IDR;
+    __IO uint32_t ODR;
+} GPIO_TypeDef;
+
+typedef struct {
+    __IO uint32_t FCR;
+    __IO uint32_t FDR;
+} FND_CNT_TypeDef;
+
+#define APB_BASEADDR 0x10000000
+#define GPIOD_BASEADDR  (APB_BASEADDR + 0x4000)
+#define FND_CNT_BASEADDR  (APB_BASEADDR + 0x5000)
+
+#define GPIOD       ((GPIO_TypeDef *) GPIOD_BASEADDR)
+#define FNDC        ((FND_CNT_TypeDef *) FND_CNT_BASEADDR)
+
+#define FNDC_ON      1
+#define FNDC_OFF     0
+
+void Switch_init(GPIO_TypeDef *GPIOx);
+uint32_t Switch_read(GPIO_TypeDef *GPIOx);
+void FNDC_init(FND_CNT_TypeDef *fndC, uint32_t ON_OFF);
+void FNDC_writeData(FND_CNT_TypeDef *fndC, uint32_t data);
+void delay(int n);
+
+int main()
+{
+    Switch_init(GPIOD);
+	uint32_t cnt = 0;
+	
+	while(1)
+    	{   
+        	FNDC_writeData(FNDC, cnt);
+        	delay(100);
+
+        	if (cnt >= 9999)
+            	cnt = 0;
+            else
+                cnt++;
+                    
+        	if (Switch_read(GPIOD) == 0x00) {
+            		FNDC_init(FNDC, FNDC_OFF);
+         	}
+         	else {
+            		FNDC_init(FNDC, FNDC_ON);
+         	}
+	}
+    	return 0;
+}
+
+void delay(int n)
+{
+    uint32_t temp = 0;
+    for (int i=0; i<n; i++){
+        for (int j=0; j<1000; j++){
+            temp++;
+        }
+    }
+}
+
+void Switch_init(GPIO_TypeDef *GPIOx)
+{
+    GPIOx-> MODER = 0x00;
+}
+uint32_t Switch_read(GPIO_TypeDef *GPIOx)
+{
+    return GPIOx-> IDR;
+}
+
+void FNDC_init(FND_CNT_TypeDef *fndC, uint32_t ON_OFF){
+    fndC->FCR = ON_OFF;
+}
+
+void FNDC_writeData(FND_CNT_TypeDef *fndC, uint32_t data){
+    fndC->FDR = data;
+}
