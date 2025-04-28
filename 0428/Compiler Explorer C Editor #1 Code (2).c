15ce263335e543331d8d090ba8b921356b7ed899
정현우 <erios5774@gmail.com>
2025-04-28 17:15:06 +0900

    Update Compiler Explorer C Editor #1 Code (2).c

--- 0428/Compiler Explorer C Editor #1 Code (2).c
+++ 0428/Compiler Explorer C Editor #1 Code (2).c
@@ -54,137 +54,6 @@ typedef struct {
 #define BTN_3       6
 #define BTN_4       7
 
-// gpio c function
-void LED_init(GPIO_TypeDef *GPIOx);
-void LED_write(GPIO_TypeDef *GPIOx, uint32_t data);
-
-// gpio d function
-void Switch_init(GPIO_TypeDef *GPIOx);
-uint32_t Switch_read(GPIO_TypeDef *GPIOx);
-
-// fnd function
-void FND_init(FND_TypeDef *fnd, uint32_t ON_OFF);
-void FND_writeCom(FND_TypeDef *fnd, uint32_t comport);
-void FND_writeData(FND_TypeDef *fnd, uint32_t data);
-
-// timer function
-void TIM_start(TIM_TypeDef *tim);
-void TIM_stop(TIM_TypeDef *tim);
-uint32_t TIM_readCounter(TIM_TypeDef *tim);
-void TIM_writePrescaler(TIM_TypeDef *tim, uint32_t psc);
-void TIM_writeAutoReload(TIM_TypeDef *tim, uint32_t arr);
-void TIM_clear(TIM_TypeDef *tim);
-
-// BTN function
-void BTN_init(GPIO_TypeDef *GPIOx);
-uint32_t BTN_getState(GPIO_TypeDef *GPIOx);
-
-// function FSM
-void func1(uint32_t *prevTime, uint32_t *data);
-void func2(uint32_t *prevTime, uint32_t *data);
-void func3(uint32_t *prevTime, uint32_t *data);
-void func4(uint32_t *prevTime, uint32_t *data);
-void power(uint32_t *prevTime, uint32_t *data);
-
-// delay function
-void delay(int n);
-
-enum { FUNC1, FUNC2, FUNC3, FUNC4};
-int main()
-{
-    uint32_t func1PrevTime = 0;
-    uint32_t func1_data = 0;
-    uint32_t func2PrevTime = 0;
-    uint32_t func2_data = 0;
-    uint32_t func3PrevTime = 0;
-    uint32_t func3_data = 0;
-    uint32_t func4PrevTime = 0;
-    uint32_t func4_data = 0;
-    uint32_t powerPrevTime = 0;
-    uint32_t power_data = 0;
-    
-    LED_init(GPIOC);
-    BTN_init(GPIOD);
-
-    TIM_writePrescaler(TIM, 100000 - 1);
-    TIM_writeAutoReload(TIM, 0xffffffff - 1);
-    TIM_start(TIM);
-    
-    uint32_t state = FUNC1;
-
-    while (1)
-    {
-        power(&powerPrevTime, &power_data);
-
-        switch(state)
-        {
-            case FUNC1:
-                func1(&func1PrevTime, &func1_data);
-            break;
-
-            case FUNC2:
-                func2(&func2PrevTime, &func2_data);
-            break;
-
-            case FUNC3:
-                func3(&func3PrevTime, &func3_data);
-            break;
-
-            case FUNC4:
-                func4(&func4PrevTime, &func4_data);
-            break;
-        }
-
-        switch(state)
-        {
-            case FUNC1:
-                if (BTN_getState(GPIOD) & (1 << BTN_2))
-                    state = FUNC2;
-                else if (BTN_getState(GPIOD) & (1 << BTN_3))
-                    state= FUNC3;
-                else if (BTN_getState(GPIOD) & (1 << BTN_4))
-                    state= FUNC4;
-                else 
-                    state = FUNC1;
-            break;
-
-            case FUNC2:
-                if (BTN_getState(GPIOD) & (1 << BTN_2))
-                    state = FUNC2;
-                else if (BTN_getState(GPIOD) & (1 << BTN_3))
-                    state= FUNC3;
-                else if (BTN_getState(GPIOD) & (1 << BTN_4))
-                    state= FUNC4;
-                else 
-                    state = FUNC1;
-            break;
-
-            case FUNC3:
-                if (BTN_getState(GPIOD) & (1 << BTN_1))
-                    state = FUNC1;
-                else if (BTN_getState(GPIOD) & (1 << BTN_3))
-                    state= FUNC3;
-                else if (BTN_getState(GPIOD) & (1 << BTN_4))
-                    state= FUNC4;
-                else 
-                    state = FUNC2;
-            break;
-
-            case FUNC4:
-                if (BTN_getState(GPIOD) & (1 << BTN_1))
-                    state = FUNC1;
-                else if (BTN_getState(GPIOD) & (1 << BTN_2))
-                    state= FUNC2;
-                else if (BTN_getState(GPIOD) & (1 << BTN_3))
-                    state= FUNC3;
-                else 
-                    state = FUNC4;
-            break;
-        }
-    }
-    return 0;
-}
-
 void delay(int n)
 {
     uint32_t temp = 0;
@@ -328,3 +197,99 @@ void power(uint32_t *prevTime, uint32_t *data)
     LED_write(GPIOC,*data);
 }
 
+enum { FUNC1, FUNC2, FUNC3, FUNC4};
+
+int main()
+{
+    uint32_t func1PrevTime = 0;
+    uint32_t func1_data = 0;
+    uint32_t func2PrevTime = 0;
+    uint32_t func2_data = 0;
+    uint32_t func3PrevTime = 0;
+    uint32_t func3_data = 0;
+    uint32_t func4PrevTime = 0;
+    uint32_t func4_data = 0;
+    uint32_t powerPrevTime = 0;
+    uint32_t power_data = 0;
+    
+    LED_init(GPIOC);
+    BTN_init(GPIOD);
+
+    TIM_writePrescaler(TIM, 100000 - 1);
+    TIM_writeAutoReload(TIM, 0xffffffff - 1);
+    TIM_start(TIM);
+    
+    uint32_t state = FUNC1;
+
+    while (1)
+    {
+        power(&powerPrevTime, &power_data);
+
+        switch(state)
+        {
+            case FUNC1:
+                func1(&func1PrevTime, &func1_data);
+            break;
+
+            case FUNC2:
+                func2(&func2PrevTime, &func2_data);
+            break;
+
+            case FUNC3:
+                func3(&func3PrevTime, &func3_data);
+            break;
+
+            case FUNC4:
+                func4(&func4PrevTime, &func4_data);
+            break;
+        }
+
+        switch(state)
+        {
+            case FUNC1:
+                if (BTN_getState(GPIOD) & (1 << BTN_2))
+                    state = FUNC2;
+                else if (BTN_getState(GPIOD) & (1 << BTN_3))
+                    state= FUNC3;
+                else if (BTN_getState(GPIOD) & (1 << BTN_4))
+                    state= FUNC4;
+                else 
+                    state = FUNC1;
+            break;
+
+            case FUNC2:
+                if (BTN_getState(GPIOD) & (1 << BTN_2))
+                    state = FUNC2;
+                else if (BTN_getState(GPIOD) & (1 << BTN_3))
+                    state= FUNC3;
+                else if (BTN_getState(GPIOD) & (1 << BTN_4))
+                    state= FUNC4;
+                else 
+                    state = FUNC1;
+            break;
+
+            case FUNC3:
+                if (BTN_getState(GPIOD) & (1 << BTN_1))
+                    state = FUNC1;
+                else if (BTN_getState(GPIOD) & (1 << BTN_3))
+                    state= FUNC3;
+                else if (BTN_getState(GPIOD) & (1 << BTN_4))
+                    state= FUNC4;
+                else 
+                    state = FUNC2;
+            break;
+
+            case FUNC4:
+                if (BTN_getState(GPIOD) & (1 << BTN_1))
+                    state = FUNC1;
+                else if (BTN_getState(GPIOD) & (1 << BTN_2))
+                    state= FUNC2;
+                else if (BTN_getState(GPIOD) & (1 << BTN_3))
+                    state= FUNC3;
+                else 
+                    state = FUNC4;
+            break;
+        }
+    }
+    return 0;
+}
